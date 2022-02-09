c52b177c5aa9156acea0f31292c919ab339b9476
vanloc1808 <vanloc1808@gmail.com>
2022-02-09 18:19:46 +0700

    string comparison

---
+++ Week05.DynamicProgramming1/sources/StringComparison.cpp
@@ -0,0 +1,19 @@
+/**
+ * Cystic Fibrosis (CF): an often fatal disease which affects the
+ * lungs, kidneys, and other organs, produces an abnormally large amount of mucus.
+ * 
+ * Mucus is a slimy material that coats epithelial surfaces and is secreted
+ * into fluids such as saliva.
+ * 
+ * Approximately 1 in 25 humans carry a faulty CF gene.
+ * When both parent carry a faulty gene, there is a 25% chance that their child will have CF.
+ */
+
+/**
+ * The Aligment Game:
+ * remove all symbols from two strings in such a way that the number of points is maximized.
+ * Remove the first symbol from both strings:
+ *      1 point if the symbols match,
+ *      0 points if they don't match.
+ * Remove the first symbol from one of the strings: 0 points.
+ */ 
