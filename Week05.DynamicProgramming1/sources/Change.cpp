0de9060ac15fd12eac95de9b68ebbf4f65db2b1d
vanloc1808 <vanloc1808@gmail.com>
2022-02-09 18:03:21 +0700

    change problem

--- Week05.DynamicProgramming1/sources/Change.cpp
+++ Week05.DynamicProgramming1/sources/Change.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Richard Bellman, 1950s, Air Force project
 int dpChange(long long money, vector<long long>& coins) {
     vector<long long> dp(money + 1, 0);
     for (long long coin : coins) {
